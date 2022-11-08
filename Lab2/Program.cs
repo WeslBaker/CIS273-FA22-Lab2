64c60101fc6e75a61ff93289e7ef866ed18902ed
WeslBaker <wesley.baker@students.fhu.edu>
2022-11-08 00:01:10 -0600

    Completed Matches() and Evaluate()

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,7 +3,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        IsBalanced("{ ( < > ) }");  // true
+        Console.WriteLine(IsBalanced("{ ( < > ) }"));  // true
         IsBalanced("<> {(})");      // false
     }
 
@@ -54,23 +54,59 @@ public class Program
 
     private static bool Matches(char closing, char opening)
     {
-        throw new NotImplementedException();
+        if((opening == '<' && closing == '>') || (opening == '(' && closing == ')') || (opening == '[' && closing == ']') || (opening == '{' && closing == '}'))
+        {
+            return true;
+        }
+        return false;
     }
 
 
     public static double? Evaluate(string s)
     {
+        Stack<double?> stack = new Stack<double?>();
+        double result = 0;
+
         // parse string into tokens
         string[] tokens = s.Split();
 
         // foreach token
         // if it's a number, push to stack
+        foreach(string token in tokens)
+        {
+            if(token.Equals("+"))
+            {
+                stack.Push(stack.Pop() + stack.Pop());
+            }
+            else if(token.Equals("-"))
+            {
+                stack.Push((stack.Pop() - stack.Pop()) * -1);
+            }
+            else if(token.Equals("*"))
+            {
+                stack.Push(stack.Pop() * stack.Pop());
+            }
+            else if(token.Equals("/"))
+            {
+                double? divisor = stack.Pop();
+                double? divided = stack.Pop();
+                stack.Push(divided / divisor);
+            }
+            else
+            {
+                stack.Push(Convert.ToInt32(token));
+            }
+        }
 
         // if it's a math operator, pop twice;
         // compute result;
         // push result onto stack
 
         // return top of stack (if the stack has 1 element)
+        if(stack.Count == 1)
+        {
+            return stack.Pop();
+        }
 
         return null;
     }
